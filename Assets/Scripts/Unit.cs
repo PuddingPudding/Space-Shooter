a6415626be6a631f17d5264d973b0f43ef9466af
PuddingPudding <hwsh70119@gmail.com>
2018-02-22 17:54:59 +0800

    初始化專案，先將所有去背的圖檔載入，另外也寫了一個名為Unit的程式，負責記錄血量攻擊與速度等，之後應該會套用至大部分的物件(玩家，敵人與障礙物)

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField] private float m_fMaxHp = 1;
+    [SerializeField] private float m_fSpeed = 5;
+    [SerializeField] private float m_fAttack = 1;
+    private float m_fCurrentHp;
+    private Rigidbody2D m_rigidbody2D;
+
+    private void Awake()
+    {
+        m_rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 _v2Dir)
+    {
+        m_rigidbody2D.velocity = _v2Dir.normalized * m_fSpeed;
+    }
+    public void Move(Vector2 _v2Dir , float _fTimes)
+    {
+        m_rigidbody2D.velocity = _v2Dir.normalized * m_fSpeed * _fTimes;
+    }
+    public void Hit(float _fDamage)
+    {
+        m_fCurrentHp -= _fDamage;
+        if(m_fCurrentHp < 0)
+        {
+            m_fCurrentHp = 0;
+        }
+    }
+}
