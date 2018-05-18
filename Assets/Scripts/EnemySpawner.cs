fea1c91224486e06404608076f1accea00cf3b2a
PuddingPudding <hwsh70119@gmail.com>
2018-05-18 15:51:04 +0800

    新增EnemySpawner來生產敵人

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform m_tEnemySpawnPoint;
+    [SerializeField] private Vector2 m_v2EnemyInterval;
+    private static EnemySpawner g_instance = null;
+
+    private void Start()
+    {
+        EnemySpawner.g_instance = this;
+    }
+
+    public static EnemySpawner GetInstance()
+    {
+        if(EnemySpawner.g_instance == null)
+        {
+            EnemySpawner.g_instance = new EnemySpawner();
+        }
+        return EnemySpawner.g_instance;
+    }
+
+    private EnemySpawner()
+    {
+    }
+}
